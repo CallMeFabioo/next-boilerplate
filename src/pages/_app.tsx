e866c931bfc5090b0e6116054e2b082fea7d01f7
Fábio de Carvalho <callmefabioo@gmail.com>
2021-04-28 23:47:01 -0300

    chore: update meta description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,13 +7,11 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Next Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <title>Next.js Boilerplate</title>
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A simple project starter to work with TypeScript, React, Next.js and Styled Components"
         />
       </Head>
       <GlobalStyles />
