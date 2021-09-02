b879b125bb88b12e88218ca6dd03486a37445426
Fábio de Carvalho <callmefabioo@gmail.com>
2021-09-02 12:48:38 -0300

    chore(deps): update dependencies, remove file generator and Storybook

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const withPWA = require('next-pwa');
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPWA({
-  future: {
-    webpack5: true
-  },
   pwa: {
     dest: 'public',
     disable: !isProd
